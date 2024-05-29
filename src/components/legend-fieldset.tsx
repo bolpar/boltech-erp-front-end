ffc8263cee0d5d6335b7a9fd43d95b5aac0757f4
Gabriel Pedroso <lucred@protonmail.com>
2024-05-29 17:06:12 -0300

    style: Label font weight to bold

--- src/components/legend-fieldset.tsx
+++ src/components/legend-fieldset.tsx
@@ -7,17 +7,19 @@ interface TitleFieldsetProps extends ComponentProps<'legend'> {
 
 export const LegendFieldset = ({ children, className }: TitleFieldsetProps) => {
   return (
-    <legend
-      className={twMerge(
-        'my-10 flex w-full items-center justify-center text-sm font-bold text-zinc-600',
-        className,
-      )}
-    >
-      <div className="w-full border-b dark:border-borderDarkMode" />
-      <h3 className="w-fit min-w-fit px-6 text-xs font-bold text-green-500">
-        {children}
-      </h3>
-      <div className="w-full border-b dark:border-borderDarkMode" />
+    <legend className="w-full">
+      <div
+        className={twMerge(
+          'col-span-full my-10 flex w-full items-center justify-center text-sm font-bold text-zinc-600',
+          className,
+        )}
+      >
+        <div className="w-full border-b dark:border-borderDarkMode" />
+        <h3 className="w-fit min-w-fit px-6 text-xs font-bold text-green-500">
+          {children}
+        </h3>
+        <div className="w-full border-b dark:border-borderDarkMode" />
+      </div>
     </legend>
   )
 }
