7be83b848ea3a914d49d153aee4e0854c41f661e
Gabriel Pedroso <lucred@protonmail.com>
2024-06-06 14:26:07 -0300

    delete: Clients data in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { getClients } from '@/services/query/getClients'
-
 export default async function Home() {
-  const clients = await getClients()
-
   return (
     <>
       <h1>Olá, mundo.</h1>
-      <div>{JSON.stringify(clients)}</div>
     </>
   )
 }
